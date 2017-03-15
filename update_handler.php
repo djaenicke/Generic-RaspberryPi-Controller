ed23d43d31d276b8824fa42bbcbd470127fbad41
Devin <djaenicke.92@gmail.com>
2017-03-15 16:21:04 -0400

    Added the remaining update handlers

--- update_handler.php
+++ update_handler.php
@@ -1,7 +1,11 @@
 <?php
     //Create a map between key names and function handlers
-    $key_handler_map = array("direction"     => "update_io_direction",
-                             "output_status" => "update_output_status");
+    $key_handler_map = array("direction"      => "update_io_direction",
+                             "output_status"  => "update_output_status",
+                             "name"           => "update_name",
+                             "pull_enable"    => "update_pull_enable",
+                             "pull_type"      => "update_pull_type",
+                             "display_enable" => "update_display_enable");
 
     //Get the id value
     if (array_key_exists('id', $_REQUEST))
@@ -41,11 +45,11 @@
         {
             if ($val == "output")
             {
-                update_database("UPDATE GPIO SET isOutput=1 WHERE id=$id");
+                update_database_integer("UPDATE GPIO SET isOutput=1 WHERE id=$id");
             }
             elseif ($val == "input")
             {
-                update_database("UPDATE GPIO SET isOutput=0 WHERE id=$id");
+                update_database_integer("UPDATE GPIO SET isOutput=0 WHERE id=$id");
             }
             else
             {
@@ -58,11 +62,77 @@
         {
             if ($val == "true")
             {
-                update_database("UPDATE GPIO SET isHigh=1 WHERE id=$id");
+                update_database_integer("UPDATE GPIO SET isHigh=1 WHERE id=$id");
             }
             elseif ($val == "false")
             {
-                update_database("UPDATE GPIO SET isHigh=0 WHERE id=$id");
+                update_database_integer("UPDATE GPIO SET isHigh=0 WHERE id=$id");
+            }
+            else
+            {
+                print("Failure");
+                exit;
+            }
+        }
+
+        static function update_name($id, $val)
+        {
+            //Establish a connection to the database
+            require 'login.php';
+            $connection = new mysqli($hostname, $username, $password, $database);
+            if ($connection->connect_error) die($connection->connect_error);
+
+            $name = $connection->real_escape_string($val);
+            $query = sprintf("UPDATE GPIO SET name='%s' WHERE id=%d", $name, $id);
+
+            $result = $connection->query($query);
+            if (!$result) die($connection->connect_error);
+
+            $connection->close();
+        }
+
+        static function update_pull_enable($id, $val)
+        {
+            if ($val == "true")
+            {
+                update_database_integer("UPDATE GPIO SET isPulled=1 WHERE id=$id");
+            }
+            elseif ($val == "false")
+            {
+                update_database_integer("UPDATE GPIO SET isPulled=0 WHERE id=$id");
+            }
+            else
+            {
+                print("Failure");
+                exit;
+            }
+        }
+
+        static function update_pull_type($id, $val)
+        {
+            //Establish a connection to the database
+            require 'login.php';
+            $connection = new mysqli($hostname, $username, $password, $database);
+            if ($connection->connect_error) die($connection->connect_error);
+
+            $pull_type = $connection->real_escape_string($val);
+            $query = sprintf("UPDATE GPIO SET pullType='%s' WHERE id=%d", $pull_type, $id);
+
+            $result = $connection->query($query);
+            if (!$result) die($connection->connect_error);
+
+            $connection->close();
+        }
+
+        static function update_display_enable($id, $val)
+        {
+            if ($val == "true")
+            {
+                update_database_integer("UPDATE GPIO SET isDisplayed=1 WHERE id=$id");
+            }
+            elseif ($val == "false")
+            {
+                update_database_integer("UPDATE GPIO SET isDisplayed=0 WHERE id=$id");
             }
             else
             {
@@ -72,7 +142,7 @@
         }
     }
 
-    function update_database($query)
+    function update_database_integer($query)
     {
         //Establish a connection to the database
         require 'login.php';
