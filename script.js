a682282ddc05406ffabbee6fea36ab26bb4a762f
Devin <djaenicke.92@gmail.com>
2017-03-15 15:14:52 -0400

    Formatting updates

--- script.js
+++ script.js
@@ -1,69 +1,80 @@
 /******************************************************************************
  * Event handler for when the dropdown is changed
  ******************************************************************************/
-var toggle_io_direction = function(e) {
-  var xmlhttp = new XMLHttpRequest();
+var toggle_io_direction = function(e)
+{
+    var xmlhttp = new XMLHttpRequest();
 
-  var chkbox = document.getElementById("control_checkbox_" + e.target.dataset.id);
+    var chkbox = document.getElementById("control_checkbox_" + e.target.dataset.id);
 
-  if(e.target.value == 'input') {
-    chkbox.disabled = true; //Disable the Off/On checkbox if configured as input
-  }
-  else {
-    chkbox.disabled = false; //Enable the Off/On checkbox if configured as output
-  }
+    if (e.target.value == 'input')
+    {
+        chkbox.disabled = true; //Disable the Off/On checkbox if configured as input
+    }
+    else
+    {
+        chkbox.disabled = false; //Enable the Off/On checkbox if configured as output
+    }
 
-  xmlhttp.open("GET", "update_handler.php?id=" + e.target.dataset.id + "&direction=" + e.target.value, true);
-  xmlhttp.send();
+    xmlhttp.open("GET", "update_handler.php?id=" + e.target.dataset.id + "&direction=" + e.target.value, true);
+    xmlhttp.send();
 
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
+    xmlhttp.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
           console.log(this.responseText);
-      }
-  };
+        }
+    };
 }
 
 /******************************************************************************
  * Event handler for when the checkbox is changed
  ******************************************************************************/
-var switch_output_state = function(e) {
-  var isHigh;
-  var state = document.getElementById("state_" + e.target.dataset.id);
+var switch_output_state = function(e)
+{
+    var isHigh;
+    var state = document.getElementById("state_" + e.target.dataset.id);
 
-  if(e.srcElement.checked) {
-    isHigh = 'true';
-    state.innerText = "High" //Make the state match the checkbox value
-  }
-  else {
-    isHigh = 'false';
-    state.innerText = "Low" //Make the state match the checkbox value
-  }
+    if (e.srcElement.checked)
+    {
+        isHigh = 'true';
+        state.innerText = "High" //Make the state match the checkbox value
+    }
+    else
+    {
+        isHigh = 'false';
+        state.innerText = "Low" //Make the state match the checkbox value
+    }
 
-  var xmlhttp = new XMLHttpRequest();
+    var xmlhttp = new XMLHttpRequest();
 
-  xmlhttp.open("GET", "update_handler.php?id=" + e.target.dataset.id + "&output_status=" + isHigh, true);
-  xmlhttp.send();
+    xmlhttp.open("GET", "update_handler.php?id=" + e.target.dataset.id + "&output_status=" + isHigh, true);
+    xmlhttp.send();
 
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          console.log(this.responseText);
-      }
-  };
+    xmlhttp.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            console.log(this.responseText);
+        }
+    };
 }
 
 /******************************************************************************
  * Function for configuring the event handlers when the document is loaded
  ******************************************************************************/
-function onDocumentLoad(){
-  var control_checkboxes = document.querySelectorAll('.control_checkbox');
+function onDocumentLoad()
+{
+    var control_checkboxes = document.querySelectorAll('.control_checkbox');
 
-  control_checkboxes.forEach(function(box){
+    control_checkboxes.forEach(function(box){
     box.addEventListener('click', switch_output_state);
-  });
+    });
 
-  var direction_selectors = document.querySelectorAll('.direction_dropdown');
+    var direction_selectors = document.querySelectorAll('.direction_dropdown');
 
-  direction_selectors.forEach(function(selector){
+    direction_selectors.forEach(function(selector){
     selector.addEventListener('change', toggle_io_direction);
-  });
+    });
 }
