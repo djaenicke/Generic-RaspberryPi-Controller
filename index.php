1116061af23c671e63ec492fc4fc1579e690e12e
Devin <djaenicke.92@gmail.com>
2017-03-15 20:58:45 -0400

    GPIO's are now only shown if set to display

--- index.php
+++ index.php
@@ -70,74 +70,76 @@
 
         <?php include "database_query.php";
         foreach($gpio_info as $current_gpio): ?>
-            <?php
-                $cnt++;
-                $gpio_num = ($current_gpio['id'] + 1); // GPIO Numbering starts at 2; add one to the ID to align them
-
-                //Determine what the initial state of the checkbox should be
-                if ($current_gpio['isHigh'])
-                {
-                    $chkboxVal = "checked";
-                    $state = "High";
-                }
-                else
-                {
-                    $chkboxVal = "";
-                    $state = "Low";
-                }
-
-                //shade every other row
-                if ($cnt % 2)
-                {
-                    $rowIsShaded = "shaded";
-                }
-                else
-                {
-                    $rowIsShaded = "";
-                }
-            ?>
-
-            <div class="output_controls <?php echo $rowIsShaded;?>">
-                <div class="alias_col"><?php echo $current_gpio['name'];?></div>
-                <div class="phys_pin_col"><?php echo $current_gpio['id'];?></div>
-
+            <?php if($current_gpio['isDisplayed']): ?>
                 <?php
-                    if ($current_gpio['isOutput'])
+                    $cnt++;
+                    $gpio_num = ($current_gpio['id'] + 1); // GPIO Numbering starts at 2; add one to the ID to align them
+
+                    //Determine what the initial state of the checkbox should be
+                    if ($current_gpio['isHigh'])
                     {
-                        $input_select = "value";
-                        $output_select = "selected value";
+                        $chkboxVal = "checked";
+                        $state = "High";
                     }
                     else
                     {
-                        $input_select = "selected value";
-                        $output_select = "value";
+                        $chkboxVal = "";
+                        $state = "Low";
                     }
-                ?>
-                <div class="direction_col">
-                    <select class="direction_dropdown" data-id="<?php echo $current_gpio['id'];?>">
-                        <option <?php echo $input_select;?>="input">Input</option>
-                        <option <?php echo $output_select;?>="output">Output</option>';
-                    </select>
-                </div>
 
-                <?php
-                    $id = sprintf("control_checkbox_%d", $current_gpio['id']);
-                    if ($current_gpio['isOutput'])
+                    //shade every other row
+                    if ($cnt % 2)
                     {
-                        $status = "";
+                        $rowIsShaded = "shaded";
                     }
                     else
                     {
-                        $status = "disabled";
+                        $rowIsShaded = "";
                     }
                 ?>
-                <div class="control_col">
-                    <input class="control_checkbox" id="<?php echo $id;?>" data-id="<?php echo $current_gpio['id'];?>" <?php echo $status;?> type="checkbox" <?php echo $chkboxVal; ?>>
-                </div>
 
-                <?php $id = sprintf("state_%d", $current_gpio['id']);?>
-                <div class="state_col" id="<?php echo $id;?>"><?php echo $state;?></div>
-            </div>
+                <div class="output_controls <?php echo $rowIsShaded;?>">
+                    <div class="alias_col"><?php echo $current_gpio['name'];?></div>
+                    <div class="phys_pin_col"><?php echo $current_gpio['id'];?></div>
+
+                    <?php
+                        if ($current_gpio['isOutput'])
+                        {
+                            $input_select = "value";
+                            $output_select = "selected value";
+                        }
+                        else
+                        {
+                            $input_select = "selected value";
+                            $output_select = "value";
+                        }
+                    ?>
+                    <div class="direction_col">
+                        <select class="direction_dropdown" data-id="<?php echo $current_gpio['id'];?>">
+                            <option <?php echo $input_select;?>="input">Input</option>
+                            <option <?php echo $output_select;?>="output">Output</option>';
+                        </select>
+                    </div>
+
+                    <?php
+                        $id = sprintf("control_checkbox_%d", $current_gpio['id']);
+                        if ($current_gpio['isOutput'])
+                        {
+                            $status = "";
+                        }
+                        else
+                        {
+                            $status = "disabled";
+                        }
+                    ?>
+                    <div class="control_col">
+                        <input class="control_checkbox" id="<?php echo $id;?>" data-id="<?php echo $current_gpio['id'];?>" <?php echo $status;?> type="checkbox" <?php echo $chkboxVal; ?>>
+                    </div>
+
+                    <?php $id = sprintf("state_%d", $current_gpio['id']);?>
+                    <div class="state_col" id="<?php echo $id;?>"><?php echo $state;?></div>
+                </div>
+            <?php endif; ?>
         <?php endforeach; ?>
 
         <br>
